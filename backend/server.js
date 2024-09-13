78478415ea17506a0aacd3cf6098adb5175da17b
Abesh <abeshsarkar2030@gmail.com>
2024-09-13 11:13:58 +0530

    cors handloe

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use(cors(corsOptions));
 
 // Middleware to explicitly set CORS headers (to handle custom issues)
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://to-do-backend-one.vercel.app/");  // Allow specific origin
+    res.header("Access-Control-Allow-Origin", "https://to-do-backend-one.vercel.app");  // Allow specific origin
     res.header("Access-Control-Allow-Credentials", "true"); // Allow credentials
     res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
